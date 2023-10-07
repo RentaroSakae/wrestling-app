4deb92c0929288a4d597873f5a649ed310cacdd6
Rentaro Sakae <r.sakae0508@gmail.com>
2023-10-07 20:21:35 +0900

    20231007OrganizerCompetitionClassController

--- app/Http/Controllers/Organizer/OrganizerCompetitionClassController.php
+++ app/Http/Controllers/Organizer/OrganizerCompetitionClassController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\CompetitionClass;
 use Illuminate\Http\Request;
 
-class CompetitionClassController extends Controller
+class OrganizerCompetitionClassController extends Controller
 {
     /**
      * Display a listing of the resource.
